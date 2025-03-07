70e2fe4295a93ec75834ec004c33226b8f9913c2
Sergey <57405954+Sergey202222@users.noreply.github.com>
2025-03-07 14:36:01 +0300

    Add files via upload

---
+++ homework2/homework3.2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+class Address
+{
+private:
+    std::string city;
+    std::string street;
+    int houseNumber;
+    int apartmentNumber;
+public:
+    Address() {}
+    Address(std::string city, std::string street, int houseNumber, int apartamentNumber) :
+        city(city),
+        street(street),
+        houseNumber(houseNumber),
+        apartmentNumber(apartamentNumber)
+    {
+    }
+    std::string get_output_address()
+    {
+        return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
+    }
+};
+
+void sort(Address* addresses, int size)
+{
+    for (int iindex{1}; iindex < size; ++iindex)
+    {
+        for (int jindex{iindex}; jindex > 0; --jindex)
+        {
+            if (addresses[jindex].get_output_address()[0] < addresses[jindex - 1].get_output_address()[0])
+            {
+                std::swap(addresses[jindex], addresses[jindex - 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int numberOfAddresses{};
+    Address* addresses{};
+    std::fstream in("in.txt");
+    if (in.is_open())
+    {
+        in >> numberOfAddresses;
+        addresses = new Address[numberOfAddresses];
+        std::string city{}, street{};
+        int houseNumber{}, apartamentNumber{};
+        for (int count{}; count < numberOfAddresses; ++count)
+        {
+            std::getline(in >> std::ws, city);
+            std::getline(in >> std::ws, street);
+            in >> houseNumber;
+            in >> apartamentNumber;
+
+            Address address(city, street, houseNumber, apartamentNumber);
+            addresses[count] = address;
+        }
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+    in.close();
+
+    sort(addresses, numberOfAddresses);
+    std::ofstream out("out.txt");
+    if (out.is_open())
+    {
+        out << numberOfAddresses << '\n';
+        for (int count{}; count < numberOfAddresses; ++count)
+        {
+            out << addresses[count].get_output_address() << '\n';
+        }
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
